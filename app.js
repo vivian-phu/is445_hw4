bd3004c7b7728fba4281aa71c181d5229eb35776
Vivian Phu <viviana.phu@gmail.com>
2020-04-14 00:18:49 -0700

    increases and decreases

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ document.addEventListener("keydown", changeSize);
 
 function changeSize() {
     var e = event.keyCode;
-    var balloonImgPX = 30;
+    var bSize = document.getElementById("balloonImg").style.fontSize;
     if (e == 73){
-        balloonImgPX = document.getElementById("balloonImg").style.fontSize = (balloonImgPX + 5) +"px";
+        console.log("you pressed i");
+        if (parseInt(bSize)>60){
+            document.getElementById("balloonImg").innerHTML = "💥";
+            document.removeEventListener('keyup', changeSize);
+        }
+        else{
+            if(bSize==""){
+                bSize=30;
+            }
+            var newBSize = parseInt(bSize) + 5;
+            console.log("increasing size to"+newBSize);
+            document.getElementById("balloonImg").style.fontSize=newBSize.toString()+"px";
+            console.log(newBSize);
+        }
+        
     }
     else if (e == 68){
-        balloonImgPX = document.getElementById("balloonImg").style.fontSize = (balloonImgPX - 10) +"px";
+        if(parseInt(bSize)==0){
+            document.getElementById("balloonImg").innerHTML = "Done";
+            document.removeEventListener('keyup', changeSize);
+        }
+        newBSize = parseInt(bSize) -10;
+        document.getElementById("balloonImg").style.fontSize = newBSize.toString() +"px";
     }
+
 }
